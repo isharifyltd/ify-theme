be5f8f9fe24695a7d678a0a10e8776af4cc16cd9
mizanureclips <mizanur.eclips@gmail.com>
2021-01-21 20:35:09 +0600

    more improvement

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,10 +3,12 @@ import React from "react";
 const Button = () => {
   return (
     <div className="Ify-btn-group">
-      <button className="Ify-btn Ify-btn-primary"> Primary</button>
-      <button className="Ify-btn Ify-btn-outline"> Outline</button>
+      <button className="Ify-btn Ify-btn-primary">Primary</button>
+      <button className="Ify-btn Ify-btn-outline">Outline</button>
       <button className="Ify-btn Ify-btn-back">Back</button>
-      <button className="Ify-btn Ify-btn-disable">Disable</button>
+      <button className="Ify-btn" disabled="true">
+        Disable
+      </button>
       <button className="Ify-btn Ify-btn-cancel">Cancel</button>
       <button className="Ify-btn Ify-btn-danger">Danger</button>
       <button className="Ify-inline-btn">Inline</button>

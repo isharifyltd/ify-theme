6c40611dd049d21faed6fc4a631da01173b4cfdf
mizanureclips <mizanur.eclips@gmail.com>
2021-01-25 12:23:44 +0600

    add new button add

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PlusIcon from "../icons/plus-icon";
 
 const Button = () => {
   return (
@@ -12,6 +13,10 @@ const Button = () => {
       <button className="Ify-btn Ify-btn-cancel">Cancel</button>
       <button className="Ify-btn Ify-btn-danger">Danger</button>
       <button className="Ify-inline-btn">Inline</button>
+      <button className="Ify-btn-add-new">
+        <PlusIcon />
+        <span>Add New</span>
+      </button>
     </div>
   );
 };
